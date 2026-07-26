bbe9a7f6ca1cc6da92cb6075fe5946f7546dd360
Tshepiso tlhong <tlhongtshepiso2@gmail.com>
2026-07-26 22:36:43 +0200

    retry after failed validation

--- prompt_engineering/validation.py
+++ prompt_engineering/validation.py
@@ -1,20 +1,34 @@
 
 
 
-valid_response = {
-    "intent": "submit_claim",
-    "confidence":0.98
-}
 
-invalid_response = "The user probably wants to submit a claim."
 
-def validate_intent(response) -> bool:
+def validate_intent(response: dict) -> bool:
 
     try:
-        return contains_fields(response) and correct_data_types(response)
-    
+        return  (contains_fields(response) and 
+                correct_data_types(response) and
+                response["intent"] in available_intents()
+                )
     except:
         return "Retrying to get valid response."
+
+
+def get_valid_intent(raw_response: dict, max_retries: int=3) -> dict:
+    current_response = raw_response
+
+    for attempt in range(max_retries):
+        if validate_intent(current_response):
+            return current_response
+        else:
+            print(f"validation failed (Attempt {attempt + 1}/{max_retries}). Restructuring...")
+            current_response = restructure_json(current_response)
+
+    return {"message": "Invalid JSON format"}
+
+def restructure_json(response) -> dict:
+    """ Request AI to fix the response to need structure"""
+    ...
 
 
 def contains_fields(response) -> bool:
@@ -28,12 +42,27 @@ def correct_data_types(response) -> bool:
         return (type(response["intent"]) == str and 
             type(response["confidence"]) == float )
     except Exception as e:
-        return False
+        return "Malformed json"
+
+
+def available_intents() -> list:
+    ...
+    return ["submit_claim", "update address"]
 
 # print("intent" in valid_response)
 # print(type(valid_response["intent"])== str)
 # print(type(valid_response["confidence"]))
 
+valid_response = {
+    "intent": "submit_claim",
+    "confidence":0.98
+}
+
+invalid_response = "The user probably wants to submit a claim."
+
+malformed_json = "{intent:"
+
 if __name__ == "__main__":
     print("Valid response:",validate_intent(valid_response))
-    print("Valid response",validate_intent(invalid_response))
+    print("Valid response:",validate_intent(invalid_response))
+    print("Valid response:",validate_intent(malformed_json))
