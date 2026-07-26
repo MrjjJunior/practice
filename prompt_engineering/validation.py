fc13d90b413606727edbef97494ca784344533c9
Tshepiso tlhong <tlhongtshepiso2@gmail.com>
2026-07-26 21:32:13 +0200

    JSON validation

---
+++ prompt_engineering/validation.py
@@ -0,0 +1,39 @@
+
+
+
+valid_response = {
+    "intent": "submit_claim",
+    "confidence":0.98
+}
+
+invalid_response = "The user probably wants to submit a claim."
+
+def validate_intent(response) -> bool:
+
+    try:
+        return contains_fields(response) and correct_data_types(response)
+    
+    except:
+        return "Retrying to get valid response."
+
+
+def contains_fields(response) -> bool:
+    if "intent" in response and "confidence" in response:
+        return True
+    else:
+        return False
+
+def correct_data_types(response) -> bool:
+    try:
+        return (type(response["intent"]) == str and 
+            type(response["confidence"]) == float )
+    except Exception as e:
+        return False
+
+# print("intent" in valid_response)
+# print(type(valid_response["intent"])== str)
+# print(type(valid_response["confidence"]))
+
+if __name__ == "__main__":
+    print(validate_intent(valid_response))
+    print(validate_intent(invalid_response))
